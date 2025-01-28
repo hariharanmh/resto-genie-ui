f6756e7aa3ae09cbfdaa2317a0e79bd8a2bffa43
imhariharanm <hariharanmh2@gmail.com>
2025-01-28 12:53:25 +0000

    Setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/resto-genie-ui",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
