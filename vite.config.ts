98b09bf2113c37f415e3f38a0b31938346626f1d
imhariharanm <hariharanmh2@gmail.com>
2025-03-10 17:10:43 +0000

    Add firebase config to project for hosting

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/resto-genie-ui",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
